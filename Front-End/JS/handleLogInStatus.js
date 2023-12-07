2b78c1d9c8b3428e84ee5f0508e47438e9c48475
C1earVision <ahmedebrahim3690@gmail.com>
2023-12-07 14:06:00 +0200

    update

--- Front-End/JS/handleLogInStatus.js
+++ Front-End/JS/handleLogInStatus.js
@@ -3,15 +3,40 @@ const signUp = document.getElementById('signUp')
 const signOut = document.getElementById('signOut')
 const userProfile = document.getElementById('userProfile')
 
+const signIn2 = document.getElementById('signIn2')
+const signUp2 = document.getElementById('signUp2')
+const signOut2 = document.getElementById('signOut2')
+const userProfile2 = document.getElementById('userProfile2')
+
+const drop_down_toggle = document.getElementById('drop-down-toggle')
+const drop_down_menu = document.getElementById('drop-down-menu')
+
+let click = true
+const toggle_menu = {true:'block',false:'none'}
+
+drop_down_toggle.addEventListener('click',()=>{
+  drop_down_menu.style.display = toggle_menu[click]
+  click = !click
+})
+
 if(localStorage.getItem('token')){
   signOut.style.display = 'block'
   userProfile.style.display = 'block'
+  signOut2.style.display = 'block'
+  userProfile2.style.display = 'block'
 }else{
   signIn.style.display = 'block'
   signUp.style.display = 'block'
+  signIn2.style.display = 'block'
+  signUp2.style.display = 'block'
 }
 
 signOut.addEventListener('click',()=>{
+  localStorage.clear()
+  location.reload()
+})
+
+signOut2.addEventListener('click',()=>{
   localStorage.clear()
   location.reload()
 })
