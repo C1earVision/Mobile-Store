48abd8712042ce45e1fa7c24ee0d70268a11a93d
C1earVision <ahmedebrahim3690@gmail.com>
2023-12-07 21:52:09 +0200

    update

--- Front-End/JS/displayProductData.js
+++ Front-End/JS/displayProductData.js
@@ -5,5 +5,5 @@ window.onload = async ()=>{
   var urlParams = new URLSearchParams(window.location.search);
   product_id = urlParams.get('product_id')
   const data = await axios.get(`https://mobilestoreapi-eo3f.onrender.com/api/v1/products/${product_id}`)
-  console.log(data)
+  div.innerHTML = data.data.product.name
 }
