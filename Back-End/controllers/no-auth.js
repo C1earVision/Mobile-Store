6d087a47500f67d3b004b7ec9ff2003da70562eb
C1earVision <ahmedebrahim3690@gmail.com>
2023-12-09 21:25:18 +0200

    update

--- Back-End/controllers/no-auth.js
+++ Back-End/controllers/no-auth.js
@@ -25,7 +25,8 @@ const getAllProducts = async (req, res)=>{
   result = result.skip(skip).limit(limit);
 
   const products = await result 
-  res.status(200).json({countAll:Products.count(), count: products.length, products})
+  const countAll = Products.estimatedDocumentCount()
+  res.status(200).json({countAll, count: products.length, products})
 }
 
 // id is inside params object
