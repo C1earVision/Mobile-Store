aa5d8a535361bc688309cbfc67a8898e6e025996
C1earVision <133123242+C1earVision@users.noreply.github.com>
2023-12-10 10:41:55 +0200

    update

--- Front-End/JS/script.js
+++ Front-End/JS/script.js
@@ -17,3 +17,29 @@ ScrollReveal().reveal('.home-content p ,.home-content h2 ', { origin: 'bottom' }
     backSpeed: 10,
     loop:true
   });
+
+
+// done by elmohamady
+function createSlick(){  
+  $(".slider").not('.slick-initialized').slick({
+    centerMode: true,
+      autoplay: true,
+      dots: true, 
+        arrows: true,
+      slidesToShow: 3,
+      responsive: [{ 
+          breakpoint: 768,
+          settings: {
+              dots: false,
+              arrows: false,
+              infinite: true,
+              slidesToShow: 1,
+              slidesToScroll: 1
+          } 
+      }]
+  }); 
+
+} 
+createSlick();
+$(window).on( 'resize', createSlick );
+
