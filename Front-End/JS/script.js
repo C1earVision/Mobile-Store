840555fb5770c637936057c4205ca5ddfb5c8eab
C1earVision <133123242+C1earVision@users.noreply.github.com>
2023-12-10 12:04:31 +0200

    update

--- Front-End/JS/script.js
+++ Front-End/JS/script.js
@@ -7,19 +7,20 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content h1 ,.home-content h3 ,.new-content h1 ,.used-content h1', { origin: 'top' });
 ScrollReveal().reveal('.home-content p ,.home-content h2 ', { origin: 'bottom' });
-  ScrollReveal().reveal('.Lefth  ', { origin: 'left' });
-  ScrollReveal().reveal(' .Righh ', { origin: 'right' });
-  
-  const typed = new Typed('.multi-text', {
-    strings: ['Unleash Innovation, Embrace Quality.'],
-    typeSpeed: 150,
-    backDelay: 2000,
-    backSpeed: 10,
-    loop:true
-  });
+ScrollReveal().reveal('.Lefth  ', { origin: 'left' });
+ScrollReveal().reveal(' .Righh ', { origin: 'right' });
+
+const typed = new Typed('.multi-text', {
+  strings: ['Unleash Innovation, Embrace Quality.'],
+  typeSpeed: 150,
+  backDelay: 2000,
+  backSpeed: 10,
+  loop:true
+});
 
 
 // done by elmohamady
+
 function createSlick(){  
   $(".slider").not('.slick-initialized').slick({
     centerMode: true,
@@ -40,21 +41,28 @@ function createSlick(){
   }); 
 
 } 
-createSlick();
 $(window).on( 'resize', createSlick );
 
 
 
 window.onload = async ()=>{
-  const new_phones_slider = document.getElementsByClassName('new-content')
-  const new_slider_div = document.createElement('div')
-  const data = await axios.get("https://mobilestoreapi-eo3f.onrender.com/api/v1/products?sort=createdAt&limit=1")
-  new_slider_div.innerHTML = 
-  `<img src="https://cdn.pixabay.com/photo/2017/09/05/23/07/dog-2719601__340.jpg">
-  <div class="desc">
-    <h2><span>Animal</span> Paintings</h2>
-    <p>$999.99</p>
-    <a href="#" class="btn">Add to Cart</a>
-  </div>`
-}  
+  const new_phones_slider = document.getElementById('new-content')
+  const data = await axios.get("https://mobilestoreapi-eo3f.onrender.com/api/v1/products?sort=-createdAt&limit=6")
+  data.data.products.map((product)=>{
+    const new_slider_div = document.createElement('div')
+    new_slider_div.innerHTML = 
+    `<img width='340px' src='./media/pokof3.jpg'>
+    <div class="desc">
+      <h2>${product.name}</h2>
+      <p>$${product.price}</p>
+      <a href="#" class="btn">Add to Cart</a>
+    </div>`
+    new_phones_slider.appendChild(new_slider_div)
+  })
+  createSlick();
+}
+
+
+
+
 
