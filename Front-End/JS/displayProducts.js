2abc639276080897ed6e67fc1528362f99c39ca8
shebov3 <shehabtarik425@yahoo.com>
2023-12-10 10:13:55 +0200

    ssjd

--- Front-End/JS/displayProducts.js
+++ Front-End/JS/displayProducts.js
@@ -146,7 +146,6 @@ search_form_2.addEventListener('submit',async (e)=>{
 window.onload = async ()=>{
   const data = await axios.get("https://mobilestoreapi-eo3f.onrender.com/api/v1/products")
   num_of_pages = Math.ceil(data.data.countAll / 12)
-  console.log(data.data.count)
   while (products_container.firstChild) {
     products_container.removeChild(products_container.firstChild);
   }
