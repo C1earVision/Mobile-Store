8549289e3ce84c757d12ad5b42fc2908410e3bdc
C1earVision <133123242+C1earVision@users.noreply.github.com>
2023-12-11 21:10:47 +0200

    update

---
+++ Back-End/models/productsUsed.js
@@ -0,0 +1,117 @@
+const mongoose = require('mongoose')
+
+const ProductsUsedSchema = new mongoose.Schema({
+  imges:{
+    img_1:{
+      type:String,
+      required: [true, 'Please provide the image for the book'],
+    },
+    img_2:{
+      type:String,
+    },
+    img_3:{
+      type:String,
+    }
+  },
+  name:{
+    type:String,
+    required: [true, 'Please provide the name of the book'],
+    unique:true,
+    minlength:5,
+  },
+  price:{
+    type:Number,
+    default: 30
+  },
+  company:{
+    type:String,
+    required: [true, 'Please provide the genres for the book'],
+  },
+  description:{
+    type:String,
+    required: [true, 'Please provide the discreption of the Phone'],
+    minlength:5,
+  },
+  specifications:{
+    body:{
+      dimensions:{
+        type:String,
+        required: [true, 'Please provide the dimensions of the phone']
+      },
+      weight:{
+        type:Number,
+        required: [true, 'Please provide the weight of the phone']
+      },
+      build:{
+        type:String,
+        required: [true, 'Please provide the build of the phone']
+      },
+      sim:{
+        type:String,
+        required: [true, 'Please provide the sim of the phone']
+      }
+    },
+    display:{
+      type:{
+        type:String,
+        required: [true, 'Please provide the type of the display']
+      },
+      size:{
+        type:String,
+        required: [true, 'Please provide the size of the display']
+      }
+    },
+    memory:{
+      type:Number,
+      required: [true, 'Please provide the memory']
+    },
+    mainCamera:{
+      features:{
+        type:String,
+        required: [true, 'Please provide the features of the camera']
+      },
+      video:{
+        type:String,
+        required: [true, 'Please provide the video specs of the camera']
+      }
+    },
+    selfieCamera:{
+      features:{
+        type:String,
+        required: [true, 'Please provide the features of the camera']
+      },
+      video:{
+        type:String,
+        required: [true, 'Please provide the video specs of the camera']
+      }
+    },
+    battery:{
+      type:Number,
+      required: [true, 'Please provide the power of the battery']
+    }
+  },
+  createdAt:{
+    type:Date,
+    default: Date.now()
+  },
+  createdBy:{
+    type:mongoose.Types.ObjectId,
+    ref:'users',
+    requierd:[true, 'Please provide user']
+  },
+  wishListedBy:{
+    type:Array,
+    default: []
+  },
+  used:{
+    type:Boolean,
+    default: true
+  },
+  soldBy:{
+    type:String,
+  }
+})
+
+
+
+module.exports = mongoose.model('ProductsUsed', ProductsUsedSchema)
