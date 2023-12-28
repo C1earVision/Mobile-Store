19850aaa9ce92c17691aa175e1bf8364e1f884e7
C1earVision <133123242+C1earVision@users.noreply.github.com>
2023-12-28 22:17:18 +0200

    update

--- Front-End/JS/buyPage.js
+++ Front-End/JS/buyPage.js
@@ -50,25 +50,30 @@ form.addEventListener('submit', async function(e){
   formData = [...formData]
   var urlParams = new URLSearchParams(window.location.search);
   const product_ids = urlParams.getAll('product_id')
-  const used = urlParams.get('used')
-  const data = {
-    user_id:localStorage.getItem('id'),
-    used:used,
-    address:{street:formData[0][1],city:formData[1][1],state:formData[2][1],zip_code:formData[3][1]},
-    items:product_ids
+  const used = urlParams.Allget('used')
+  const data = []
+  for(let i=0; i<product_ids.length;i++){
+    data.push({
+      user_id:localStorage.getItem('id'),
+      used:used[i],
+      address:{street:formData[0][1],city:formData[1][1],state:formData[2][1],zip_code:formData[3][1]},
+      items:product_ids[i]
+    })
   }
-  const product = await axios.
-  request({
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-    data: data,
-    method: "POST",
-    url: `https://mobilestoreapi-eo3f.onrender.com/api/v1/user/checkout?used=${used}`,
-  }).then((res)=>{
-    console.log(res)
-    // alert('Product added successfully')
-  }).catch((res)=>{
-    console.log(res)
+  data.map(async (product)=>{
+    const product = await axios.
+    request({
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      data: data,
+      method: "POST",
+      url: `https://mobilestoreapi-eo3f.onrender.com/api/v1/user/checkout?used=${used}`,
+    }).then((res)=>{
+      console.log(res)
+    }).catch((res)=>{
+      console.log(res)
+    })
   })
+
 })
